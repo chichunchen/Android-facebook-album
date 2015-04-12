e08be25ce7e1514b33948d0f291d1ac7c8a00ccc
chichunchen <chi.chun_chen844@icloud.com>
2015-04-12 21:07:30 +0800

    Set up google map service backbone.

---
+++ app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MainActivity.java
@@ -0,0 +1,13 @@
+package com.project.android.nctu.facebook_googlemap_album;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
