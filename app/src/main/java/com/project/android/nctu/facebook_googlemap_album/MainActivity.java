149af2c7a004b6b115be3cb6264da36276d41b06
chichunchen <chi.chun_chen844@icloud.com>
2015-04-12 22:56:46 +0800

    Create map activity.

--- app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MainActivity.java
+++ app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MainActivity.java
@@ -1,7 +1,9 @@
 package com.project.android.nctu.facebook_googlemap_album;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewMap(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 }
