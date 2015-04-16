76bcc7f65d231475cad4443b9f3d15700392f14a
chichunchen <chi.chun_chen844@icloud.com>
2015-04-17 03:13:13 +0800

    Implement take photo button.

--- app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MapActivity.java
+++ app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MapActivity.java
@@ -8,12 +8,23 @@ import com.google.android.gms.maps.model.*;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class MapActivity extends Activity implements OnMapReadyCallback {
 
     private String query;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +33,12 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
 
 
         // test
-        Intent intent = getIntent();
-        query = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        Context context = getApplicationContext();
-        Toast toast = Toast.makeText(context, query, Toast.LENGTH_LONG);
-        toast.show();
+//        Intent intent = getIntent();
+//        query = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+//
+//        Context context = getApplicationContext();
+//        Toast toast = Toast.makeText(context, query, Toast.LENGTH_LONG);
+//        toast.show();
 
 
         // map
@@ -48,5 +59,32 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
                 .title(query)
                 .snippet("Welcome to fb album.")
                 .position(current_place));
+    }
+
+    public void takePhoto(View view) {
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File tmpFile = new File(Environment.getExternalStorageDirectory(),"image.jpg");
+        Uri outputFileUri = Uri.fromFile(tmpFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            // Bitmap bitmap = BitmapFactory.decodeFile(Environment
+            //         .getExternalStorageDirectory() + "/image.jpg");
+            // .setImageBitmap(bitmap);
+        }
+    }
+
+    public void shareInfo(View view) {
+        // TODO share information
     }
 }
