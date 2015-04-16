dcb86e281448da7c81d51b194eb9fd63ce582005
chichunchen <chi.chun_chen844@icloud.com>
2015-04-17 06:31:14 +0800

    Add gps postition class.

---
+++ app/src/main/java/com/project/android/nctu/facebook_googlemap_album/GpsInfo.java
@@ -0,0 +1,66 @@
+package com.project.android.nctu.facebook_googlemap_album;
+
+/**
+ * Created by chichunchen on 4/17/15.
+ */
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class GpsInfo implements LocationListener {
+    private LocationManager locationManager;
+
+    public double lat = 0, lon = 0, speed = -1;
+
+    public GpsInfo(LocationManager locationManager){
+        this.locationManager = locationManager;
+
+        try{
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, GpsInfo.this);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, GpsInfo.this);
+
+            String bestProvider = LocationManager.GPS_PROVIDER;
+            Criteria criteria = new Criteria();
+            bestProvider = this.locationManager.getBestProvider(criteria, true);
+            Location location = this.locationManager.getLastKnownLocation(bestProvider);
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+            speed = location.getSpeed();
+        }
+        catch(Exception e){
+            lat = 0;
+            lon = 0;
+            speed = -1;
+        }
+    }
+
+
+    public void getLocationInfo(int count, String dateTime){
+        String content = count+"\t"+dateTime+"\t"+lat+"\t"+lon+"\t"+speed+"\n";
+    }
+
+    public void removeUpdates(){
+        locationManager.removeUpdates(GpsInfo.this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lon = location.getLongitude();
+        speed = location.getSpeed();
+    }
+
+    @Override
+    public void onProviderDisabled(String arg0) {
+    }
+
+    @Override
+    public void onProviderEnabled(String arg0) {
+    }
+
+    @Override
+    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+    }
+}
--- app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MapActivity.java
+++ app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MapActivity.java
@@ -1,7 +1,5 @@
 package com.project.android.nctu.facebook_googlemap_album;
 
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
@@ -10,10 +8,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -21,10 +25,12 @@ import android.widget.Toast;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 
 public class MapActivity extends Activity implements OnMapReadyCallback {
 
     private String query;
+    private GpsInfo gpsInfo;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView mImageView;
 
@@ -33,15 +39,17 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
 
+        // init gps
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        gpsInfo = new GpsInfo(locationManager);
 
         // test
-//        Intent intent = getIntent();
-//        query = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-//
-//        Context context = getApplicationContext();
-//        Toast toast = Toast.makeText(context, query, Toast.LENGTH_LONG);
-//        toast.show();
+        Intent intent = getIntent();
+        query = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, query + "lat: " + gpsInfo.lat + "/ lon: " + gpsInfo.lon, Toast.LENGTH_SHORT);
+        toast.show();
 
         // map
         MapFragment mapFragment = (MapFragment) getFragmentManager()
@@ -52,7 +60,7 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap map) {
         LatLng current_place = new LatLng(25.0336, 121.5650);
-        // LatLng place = (LatLng)PlacePicker.getPlace(query, this);
+        // LatLng current_place = new LatLng(gpsInfo.lon, gpsInfo.lat);
 
         map.setMyLocationEnabled(true);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(current_place, 13));
@@ -62,6 +70,8 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
                 .snippet("Welcome to fb album.")
                 .position(current_place));
     }
+
+    // TODO gpsInfo.removeUpdates();
 
     public void takePhoto(View view) {
         dispatchTakePictureIntent();
@@ -73,7 +83,7 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         String strDate = sdf.format(c.getTime());
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File tmpFile = new File(Environment.getExternalStorageDirectory(),"Pictures/fbcheckin" + strDate + ".jpg");
+        File tmpFile = new File(Environment.getExternalStorageDirectory(),"Pictures/fbcheckin-" + strDate + ".jpg");
         Uri outputFileUri = Uri.fromFile(tmpFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
         startActivityForResult(intent, 0);
