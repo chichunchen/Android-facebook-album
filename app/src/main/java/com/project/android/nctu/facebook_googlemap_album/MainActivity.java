d5078b366a60ae955b628fed97fcb296751a4e13
chichunchen <chi.chun_chen844@icloud.com>
2015-04-17 04:08:15 +0800

    Add bar to main activity.

--- app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MainActivity.java
+++ app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MainActivity.java
@@ -1,12 +1,16 @@
 package com.project.android.nctu.facebook_googlemap_album;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     public static final String EXTRA_MESSAGE = "com.project.android.nctu.facebook_googlemap_album.INPUT";
 
@@ -33,4 +37,24 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // TODO settings
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
