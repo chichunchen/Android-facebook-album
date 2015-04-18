33e312ed3200a27c95cca73f7bf8511813cc3b51
harryron9 <harryron9.iem00@nctu.edu.tw>
2015-04-18 19:11:06 +0800

    fb login done maybe

--- app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MainActivity.java
+++ app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MainActivity.java
@@ -2,18 +2,106 @@ package com.project.android.nctu.facebook_googlemap_album;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+/*
 public class MainActivity extends ActionBarActivity {
 
+    public static final int INDEX_SIMPLE_LOGIN = 0;
+    public static final int INDEX_CUSTOM_LOGIN = 1;
+
+    private static final String STATE_SELECTED_FRAGMENT_INDEX = "selected_fragment_index";
+    public static final String FRAGMENT_TAG = "fragment_tag";
+    private FragmentManager mFragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFragmentManager = getSupportFragmentManager();
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.action_simple_login) {
+            toggleFragment(INDEX_SIMPLE_LOGIN);
+            return true;
+        }
+        if (id == R.id.action_custom_login) {
+            toggleFragment(INDEX_CUSTOM_LOGIN);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toggleFragment(int index) {
+        Fragment fragment = mFragmentManager.findFragmentByTag(FRAGMENT_TAG);
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        switch (index){
+            case INDEX_SIMPLE_LOGIN:
+                transaction.replace(android.R.id.content, new FragmentSimpleLoginButton(),FRAGMENT_TAG);
+                break;
+            case INDEX_CUSTOM_LOGIN:
+                transaction.replace(android.R.id.content, new FragmentCustomLoginButton(),FRAGMENT_TAG);
+                break;
+        }
+        transaction.commit();
+    }
+}
+*/
+public class MainActivity extends ActionBarActivity {
+
+    CallbackManager callbackManager;
+    private AccessToken accessToken;
 
     public static final int INDEX_SIMPLE_LOGIN = 0;
     public static final int INDEX_CUSTOM_LOGIN = 1;
@@ -26,11 +114,103 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //宣告callback Manager
+        callbackManager = CallbackManager.Factory.create();
+
+        //找到button
+        Button loginButton = (Button) findViewById(R.id.fb_login);
+
+        loginButton.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile", "user_friends"));
+            }
+        });
+
+
+        //幫 LoginManager 增加callback function
+        //這邊為了方便 直接寫成inner class
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            //登入成功
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                //accessToken之後或許還會用到 先存起來
+                accessToken = loginResult.getAccessToken();
+
+                Log.d("FB", "access token got.");
+
+                //send request and call graph api
+                GraphRequest request = GraphRequest.newMeRequest(
+                        accessToken,
+                        new GraphRequest.GraphJSONObjectCallback() {
+
+                            //當RESPONSE回來的時候
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                                //讀出姓名 ID FB個人頁面連結
+                                Log.d("FB", "complete");
+                                Log.d("FB", object.optString("name"));
+                                Log.d("FB", object.optString("link"));
+                                Log.d("FB", object.optString("id"));
+
+                            }
+                        });
+
+                //包入你想要得到的資料 送出request
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,link");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            //登入取消
+            @Override
+            public void onCancel() {
+                // App code
+                Log.d("FB", "CANCEL");
+            }
+
+            //登入失敗
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.d("FB", exception.toString());
+            }
+        });
+
+
+
         mFragmentManager = getSupportFragmentManager();
     }
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
     public void viewMap(View view) {
         Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
@@ -40,6 +220,7 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }*/
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
       super.onSaveInstanceState(outState);
