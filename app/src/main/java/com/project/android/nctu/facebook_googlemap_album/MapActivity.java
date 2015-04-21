3666349a6f4eb9a8d64cc0d21971e870b57f6f84
chichunchen <chi.chun_chen844@icloud.com>
2015-04-22 02:27:08 +0800

    Plotting markers on a map in Android using data from a JSON web service.

--- app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MapActivity.java
+++ app/src/main/java/com/project/android/nctu/facebook_googlemap_album/MapActivity.java
@@ -3,34 +3,38 @@ package com.project.android.nctu.facebook_googlemap_album;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Address;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
-public class MapActivity extends Activity implements OnMapReadyCallback {
+public class MapActivity extends FragmentActivity {
 
-    private String query;
-    private GpsInfo gpsInfo;
+    private static final String SERVICE_URL = "http://140.113.122.162:3000/checkins.json";
+    private final String LOG_TAG = "Map Activity";
+    protected GoogleMap map;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView mImageView;
 
@@ -39,35 +43,96 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
 
-        // init gps
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        gpsInfo = new GpsInfo(locationManager);
-
-//        Context context = getApplicationContext();
-//        Toast toast = Toast.makeText(context, query + "lat: " + gpsInfo.lat + "/ lon: " + gpsInfo.lon, Toast.LENGTH_SHORT);
-//        toast.show();
-
-        // map
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        setUpMapIfNeeded();
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        LatLng current_place = new LatLng(25.0336, 121.5650);
-        // LatLng current_place = new LatLng(gpsInfo.lon, gpsInfo.lat);
-
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(current_place, 13));
-
-        map.addMarker(new MarkerOptions()
-                .title(query)
-                .snippet("Welcome to fb album.")
-                .position(current_place));
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
     }
 
-    // TODO gpsInfo.removeUpdates();
+    private void setUpMapIfNeeded() {
+        if (map == null) {
+            map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            if (map != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+
+        map.setMyLocationEnabled(true);
+
+        // Retrieve the checkins data from the web service
+        // In a worker thread since it's a network operation.
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    retrieveAndAddCheckins();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "Cannot retrive checkins", e);
+                    return;
+                }
+            }
+        }).start();
+    }
+
+    protected void retrieveAndAddCheckins() throws IOException {
+        HttpURLConnection conn = null;
+        final StringBuilder json = new StringBuilder();
+        try {
+            // Connect to the web service
+            URL url = new URL(SERVICE_URL);
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            // Read the JSON data into the StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                json.append(buff, 0, read);
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error connecting to service", e);
+            throw new IOException("Error connecting to service", e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        // Create markers for the city data.
+        // Must run this on the UI thread since it's a UI operation.
+        runOnUiThread(new Runnable() {
+            public void run() {
+                try {
+                    createMarkersFromJson(json.toString());
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, "Error processing JSON", e);
+                }
+            }
+        });
+    }
+
+    void createMarkersFromJson(String json) throws JSONException {
+        // De-serialize the JSON string into an array of city objects
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            // Create a marker for each city in the JSON data.
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            map.addMarker(new MarkerOptions()
+                            .title(jsonObj.getString("title"))
+                            .snippet(jsonObj.getString("address"))
+                            .position(new LatLng(
+                                    Double.valueOf(jsonObj.getString("latitude")),
+                                    Double.valueOf(jsonObj.getString("longitude"))
+                            ))
+            );
+        }
+    }
 
     public void takePhoto(View view) {
         dispatchTakePictureIntent();
